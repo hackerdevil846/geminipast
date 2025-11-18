dc1b4d29a8c9b2a88bd4364786b4d44821069382
Mastermind-svg <jantera239@gmail.com>
2025-11-18 15:56:18 +0600

    Update upscale.js

--- scripts/cmds/upscale.js
+++ scripts/cmds/upscale.js
@@ -4,7 +4,7 @@ const FormData = require("form-data");
 module.exports = {
   config: {
     name: "upscale",
-    aliases: ["enhanced", "hdphoto"],
+    aliases: [],
     version: "1.1",
     author: "𝐴𝑠𝑖𝑓 𝑀𝑎ℎ𝑚𝑢𝑑",
     role: 0,
