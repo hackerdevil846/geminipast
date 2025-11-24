8bb659fd806cde8248b6e5b72cfec6c8ee5df1bd
Mastermind-svg <jantera239@gmail.com>
2025-11-25 00:04:08 +0600

    Update globalData.js

--- database/controller/globalData.js
+++ database/controller/globalData.js
@@ -40,14 +40,15 @@ module.exports = async function (databaseType, globalModel, fakeGraphql) {
 		Global = [];
 	}
 
-	global.db.globalData = Global;
+	// FIX: Renamed to avoid conflict with controller object
+	global.db.allGlobalData = Global;
 
 	async function save(key, data, mode, path) {
 		try {
-			let index = _.findIndex(global.db.globalData, { key });
+			let index = _.findIndex(global.db.allGlobalData, { key });
 			if (index === -1 && mode === "update") {
 				await create(key, {});
-				index = _.findIndex(global.db.globalData, { key });
+				index = _.findIndex(global.db.allGlobalData, { key });
 			}
 
 			switch (mode) {
@@ -55,24 +56,23 @@ module.exports = async function (databaseType, globalModel, fakeGraphql) {
 					if (databaseType === "mongodb" || databaseType === "sqlite") {
 						let dataCreated = await globalModel.create({ key, data });
 						dataCreated = databaseType === "mongodb" ? _.omit(dataCreated._doc, ["_id", "__v"]) : dataCreated.get({ plain: true });
-						global.db.globalData.push(dataCreated);
+						global.db.allGlobalData.push(dataCreated);
 						return _.cloneDeep(dataCreated);
 					} else if (databaseType === "json") {
 						const newData = { key, data };
-						global.db.globalData.push(newData);
-						writeJsonSync(pathGlobalData, global.db.globalData, optionsWriteJSON);
+						global.db.allGlobalData.push(newData);
+						writeJsonSync(pathGlobalData, global.db.allGlobalData, optionsWriteJSON);
 						return _.cloneDeep(newData);
 					}
 					break;
 				case "update":
 					if (index === -1) return null;
-					const oldData = global.db.globalData[index];
+					const oldData = global.db.allGlobalData[index];
 					const dataWillChange = {};
 
 					if (path && (typeof path === "string" || Array.isArray(path))) {
 						const rootKey = Array.isArray(path) ? path[0] : path.split(".")[0];
-						dataWillChange[rootKey] = oldData.data[rootKey]; 
-						// Deep set logic handled below by merging
+						// dataWillChange[rootKey] = oldData.data[rootKey]; 
 					}
 
 					const finalData = _.cloneDeep(oldData.data);
@@ -82,25 +82,25 @@ module.exports = async function (databaseType, globalModel, fakeGraphql) {
 					if (databaseType === "mongodb") {
 						let dataUpdated = await globalModel.findOneAndUpdate({ key }, { data: finalData }, { returnDocument: 'after' });
 						dataUpdated = _.omit(dataUpdated._doc, ["_id", "__v"]);
-						global.db.globalData[index] = dataUpdated;
+						global.db.allGlobalData[index] = dataUpdated;
 						return _.cloneDeep(dataUpdated);
 					} else if (databaseType === "sqlite") {
 						const item = await globalModel.findOne({ where: { key } });
 						const dataUpdated = (await item.update({ data: finalData })).get({ plain: true });
-						global.db.globalData[index] = dataUpdated;
+						global.db.allGlobalData[index] = dataUpdated;
 						return _.cloneDeep(dataUpdated);
 					} else {
-						global.db.globalData[index].data = finalData;
-						writeJsonSync(pathGlobalData, global.db.globalData, optionsWriteJSON);
-						return _.cloneDeep(global.db.globalData[index]);
+						global.db.allGlobalData[index].data = finalData;
+						writeJsonSync(pathGlobalData, global.db.allGlobalData, optionsWriteJSON);
+						return _.cloneDeep(global.db.allGlobalData[index]);
 					}
 					break;
 				case "remove":
 					if (index != -1) {
-						global.db.globalData.splice(index, 1);
+						global.db.allGlobalData.splice(index, 1);
 						if (databaseType === "mongodb") await globalModel.deleteOne({ key });
 						else if (databaseType === "sqlite") await globalModel.destroy({ where: { key } });
-						else writeJsonSync(pathGlobalData, global.db.globalData, optionsWriteJSON);
+						else writeJsonSync(pathGlobalData, global.db.allGlobalData, optionsWriteJSON);
 					}
 					break;
 			}
@@ -112,8 +112,8 @@ module.exports = async function (databaseType, globalModel, fakeGraphql) {
 		return new Promise((resolve, reject) => {
 			taskQueue.push(async () => {
 				try {
-					if (global.db.globalData.some(i => i.key == key)) {
-						return resolve(_.cloneDeep(global.db.globalData.find(i => i.key == key)));
+					if (global.db.allGlobalData.some(i => i.key == key)) {
+						return resolve(_.cloneDeep(global.db.allGlobalData.find(i => i.key == key)));
 					}
 					const result = await save(key, data, "create");
 					resolve(result);
@@ -126,7 +126,7 @@ module.exports = async function (databaseType, globalModel, fakeGraphql) {
 		return new Promise((resolve, reject) => {
 			taskQueue.push(async () => {
 				try {
-					let data = global.db.globalData.find(i => i.key == key);
+					let data = global.db.allGlobalData.find(i => i.key == key);
 					if (!data) data = await create(key, {});
 					
 					if (query) data = fakeGraphql(query, data);
