622d8903b64d912bb45d3a6996e58e2b03523212
hackerdevil846 <hackerdevil846@gmail.com>
2025-11-24 21:34:38 +0600

    Refactor socketIO module for improved connection handling

--- bot/login/socketIO.js
+++ bot/login/socketIO.js
@@ -1,45 +1,24 @@
-// This module will be called if enabled in the config (severUptime.socket.enable = true)
-/**
- * @example for connect to socket.io
- * view file ./connectSocketIO.example.js
- */
-const { Server } = require("socket.io");
 const { log, getText } = global.utils;
-const { config } = global.GoatBot;
+const socketIO = require("socket.io");
 
-module.exports = async (server) => {
-	const { channelName, verifyToken } = config.serverUptime.socket;
-	let io;
-
+module.exports = function (server) {
 	try {
-		if (!channelName)
-			throw ('"channelName" is not defined in config');
-		if (!verifyToken)
-			throw ('"verifyToken" is not defined in config');
-		io = new Server(server);
-		log.info("SOCKET IO", getText("socketIO", "connected"));
-	}
-	catch (err) {
-		return log.err("SOCKET IO", getText("socketIO", "error"), err);
-	}
+		const io = socketIO(server);
+		
+		io.on("connection", (socket) => {
+			// log.info("SOCKET.IO", "New client connected: " + socket.id);
 
-	io.on("connection", (socket) => {
-		if (socket.handshake.query.verifyToken != verifyToken) {
-			io.to(socket.id).emit(channelName, {
-				status: "error",
-				message: "Token is invalid"
+			socket.on("disconnect", () => {
+				// log.info("SOCKET.IO", "Client disconnected: " + socket.id);
 			});
-			socket.disconnect();
-			return;
-		}
-		log.info("SOCKET IO", `New client connected to socket: ${socket.id}`);
-		io.to(socket.id).emit(channelName, {
-			status: "success",
-			message: "Connected to server successfully"
-		});
-		socket.on("disconnect", () => {
-			log.info("SOCKET IO", `Client disconnected from socket: ${socket.id}`);
-		});
-	});
-};
 
+			socket.on("error", (err) => {
+				log.warn("SOCKET.IO", `Socket error: ${err.message}`);
+			});
+		});
+
+		log.info("SOCKET.IO", getText('socketIO', 'connected'));
+	} catch (err) {
+		log.err("SOCKET.IO", getText('socketIO', 'error'), err);
+	}
+};
