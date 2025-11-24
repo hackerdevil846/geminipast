b9adb672777fa25d2d9ec48e9a422547b7eece39
hackerdevil846 <hackerdevil846@gmail.com>
2025-11-24 21:28:07 +0600

    Update logColor.js

--- logger/logColor.js
+++ logger/logColor.js
@@ -1,2 +1,9 @@
 const { colors } = require('../func/colors.js');
-module.exports = (color, message) => console.log(colors.hex(color, message));
+
+module.exports = (color, message) => {
+    if (colors && typeof colors.hex === 'function') {
+        console.log(colors.hex(color, message));
+    } else {
+        console.log(message); // Fallback if colors func fails
+    }
+};
