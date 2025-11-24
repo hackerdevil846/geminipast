887fa0de7602dfe98b7293c7d4f32d5fbfcd3b11
Mastermind-svg <jantera239@gmail.com>
2025-11-25 01:19:07 +0600

    Delete update.js

--- update.js
+++
@@ -1,4 +0,0 @@
-const { log } = require('./logger/log.js');
-
-// Prevent loading remote updater which would break the bot
-log.warn("UPDATER", "Update script disabled to protect Super Modified core files.");
