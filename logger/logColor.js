38982de1d45c679c7f3507144c3084c59edda9d7
Mastermind-svg <jantera239@gmail.com>
2025-11-25 01:25:33 +0600

    Delete logger directory

--- logger/logColor.js
+++
@@ -1,9 +0,0 @@
-const { colors } = require('../func/colors.js');
-
-module.exports = (color, message) => {
-    if (colors && typeof colors.hex === 'function') {
-        console.log(colors.hex(color, message));
-    } else {
-        console.log(message); // Fallback if colors func fails
-    }
-};
