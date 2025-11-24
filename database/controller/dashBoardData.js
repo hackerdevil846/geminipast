c61886f21516a8573a16631f12ef54e8a5fc6777
Mastermind-svg <jantera239@gmail.com>
2025-11-25 00:04:38 +0600

    Update dashBoardData.js

--- database/controller/dashBoardData.js
+++ database/controller/dashBoardData.js
@@ -40,11 +40,12 @@ module.exports = async function (databaseType, dashBoardModel, fakeGraphql) {
 		DashBoardData = [];
 	}
 
-	global.db.dashBoardData = DashBoardData;
+	// FIX: Renamed to avoid conflict
+	global.db.allDashBoardData = DashBoardData;
 
 	async function save(email, userData, mode) {
 		try {
-			const index = _.findIndex(global.db.dashBoardData, { email });
+			const index = _.findIndex(global.db.allDashBoardData, { email });
 			if (index === -1 && mode === "update") {
 				throw new CustomError({ name: "USER_NOT_FOUND", message: "User not found in dashboard database" });
 			}
@@ -54,40 +55,40 @@ module.exports = async function (databaseType, dashBoardModel, fakeGraphql) {
 					if (databaseType === "mongodb" || databaseType === "sqlite") {
 						let dataCreated = await dashBoardModel.create(userData);
 						dataCreated = databaseType === "mongodb" ? _.omit(dataCreated._doc, ["_id", "__v"]) : dataCreated.get({ plain: true });
-						global.db.dashBoardData.push(dataCreated);
+						global.db.allDashBoardData.push(dataCreated);
 						return _.cloneDeep(dataCreated);
 					} else if (databaseType === "json") {
-						global.db.dashBoardData.push(userData);
-						writeJsonSync(pathDashBoardData, global.db.dashBoardData, optionsWriteJSON);
+						global.db.allDashBoardData.push(userData);
+						writeJsonSync(pathDashBoardData, global.db.allDashBoardData, optionsWriteJSON);
 						return _.cloneDeep(userData);
 					}
 					break;
 				case "update":
-					const oldData = global.db.dashBoardData[index];
+					const oldData = global.db.allDashBoardData[index];
 					const dataWillChange = { ...oldData, ...userData };
 
 					if (databaseType === "mongodb") {
 						let dataUpdated = await dashBoardModel.findOneAndUpdate({ email }, userData, { returnDocument: 'after' });
 						dataUpdated = _.omit(dataUpdated._doc, ["_id", "__v"]);
-						global.db.dashBoardData[index] = dataUpdated;
+						global.db.allDashBoardData[index] = dataUpdated;
 						return _.cloneDeep(dataUpdated);
 					} else if (databaseType === "sqlite") {
 						const user = await dashBoardModel.findOne({ where: { email } });
 						const dataUpdated = (await user.update(userData)).get({ plain: true });
-						global.db.dashBoardData[index] = dataUpdated;
+						global.db.allDashBoardData[index] = dataUpdated;
 						return _.cloneDeep(dataUpdated);
 					} else {
-						global.db.dashBoardData[index] = dataWillChange;
-						writeJsonSync(pathDashBoardData, global.db.dashBoardData, optionsWriteJSON);
+						global.db.allDashBoardData[index] = dataWillChange;
+						writeJsonSync(pathDashBoardData, global.db.allDashBoardData, optionsWriteJSON);
 						return _.cloneDeep(dataWillChange);
 					}
 					break;
 				case "remove":
 					if (index != -1) {
-						global.db.dashBoardData.splice(index, 1);
+						global.db.allDashBoardData.splice(index, 1);
 						if (databaseType === "mongodb") await dashBoardModel.deleteOne({ email });
 						else if (databaseType === "sqlite") await dashBoardModel.destroy({ where: { email } });
-						else writeJsonSync(pathDashBoardData, global.db.dashBoardData, optionsWriteJSON);
+						else writeJsonSync(pathDashBoardData, global.db.allDashBoardData, optionsWriteJSON);
 					}
 					break;
 			}
@@ -99,7 +100,7 @@ module.exports = async function (databaseType, dashBoardModel, fakeGraphql) {
 		return new Promise((resolve, reject) => {
 			taskQueue.push(async () => {
 				try {
-					if (global.db.dashBoardData.some(u => u.email == userData.email)) {
+					if (global.db.allDashBoardData.some(u => u.email == userData.email)) {
 						throw new CustomError({ name: "EMAIL_EXISTS", message: "Email already exists" });
 					}
 					const result = await save(userData.email, userData, "create");
@@ -113,7 +114,7 @@ module.exports = async function (databaseType, dashBoardModel, fakeGraphql) {
 		return new Promise((resolve, reject) => {
 			taskQueue.push(async () => {
 				try {
-					const data = global.db.dashBoardData.find(u => u.email == email);
+					const data = global.db.allDashBoardData.find(u => u.email == email);
 					resolve(data ? _.cloneDeep(data) : null);
 				} catch (e) { reject(e); }
 			});
