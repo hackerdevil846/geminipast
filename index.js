9f8000ac030b3c480d392545c715c66a7c7718e9
Mastermind-svg <jantera239@gmail.com>
2025-11-25 04:34:35 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,19 +2,6 @@
  * @author NTKhang
  * ! The source code is written by NTKhang, please don't change the author's name everywhere. Thank you for using
  * ! Official source code: https://github.com/ntkhang03/Goat-Bot-V2
- * ! If you do not download the source code from the above address, you are using an unknown version and at risk of having your account hacked
- *
- * English:
- * ! Please do not change the below code, it is very important for the project.
- * It is my motivation to maintain and develop the project for free.
- * ! If you change it, you will be banned forever
- * Thank you for using
- *
- * Vietnamese:
- * ! Vui lòng không thay đổi mã bên dưới, nó rất quan trọng đối với dự án.
- * Nó là động lực để tôi duy trì và phát triển dự án miễn phí.
- * ! Nếu thay đổi nó, bạn sẽ bị cấm vĩnh viễn
- * Cảm ơn bạn đã sử dụng
  */
 
 const { spawn } = require("child_process");
@@ -28,11 +15,20 @@ function startProject() {
 	});
 
 	child.on("close", (code) => {
-		if (code == 2) {
-			log.info("Restarting Project...");
+		if (code === 2) {
+            // Restart triggered by bot logic (e.g., login failure or update)
+			log.info("RESTART", "Restarting Project...");
 			startProject();
-		}
+		} else if (code !== 0) {
+            // Crash detected
+            log.error("CRASH", `Child process exited with code ${code}. Restarting in 5 seconds...`);
+            setTimeout(() => startProject(), 5000);
+        }
 	});
+    
+    child.on("error", (err) => {
+        log.error("SYSTEM", "Failed to start child process.", err);
+    });
 }
 
 startProject();
